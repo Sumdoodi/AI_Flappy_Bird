f013a38c9f4587315241c97b487014fec0f05ab4
Jonah <mrminecraft7026@gmail.com>
2022-03-24 11:09:39 -0400

    Flappy Bird Unity + gitignore

---
+++ AI_Group_Assignment/Assets/Scripts/Pipes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipes : MonoBehaviour
+{
+    public float speed = 10; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.position -= new Vector3(Time.deltaTime * speed, 0, 0);
+
+        if(this.gameObject.transform.position.x < -5)
+        {
+            Destroy(this);
+        }
+    }
+}
